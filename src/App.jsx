4cc600e3a82fc63e2d72494ca970f2a79e253f11
Rodrigo <76666085+RodrigoDSVDF@users.noreply.github.com>
2025-09-16 12:09:28 -0300

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -276,7 +276,7 @@ function App() {
                   <Key className="w-10 h-10 text-white" />
                 </div>
                 <h3 className="text-2xl font-semibold text-white mb-4">Vantagem Competitiva</h3>
-                <p className="text-gray-300 text-lg">Destaque-se em seus projetos</p>
+                <p className="text-gray-300 text-lg">Seja a pessoa que entrega mais rápido, com mais qualidade e ideias mais inovadoras. Aplique todo o ecossistema NEXUS e torne-se referência absoluta em sua área, seja no trabalho, nos estudos ou em projetos pessoais</p>
               </div>
             </div>
           </div>
