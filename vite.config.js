f89a40bab039c8427feb68241c32aee6a1680e16
Rodrigo <76666085+RodrigoDSVDF@users.noreply.github.com>
2025-10-27 11:53:44 -0300

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -1,12 +1,12 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
-import tailwindcss from '@tailwindcss/vite'
+import tailwindcss from '@tailwindcss/vite' // (Presumindo que @tailwindcss/vite esteja instalado)
 import path from 'path'
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [react(),tailwindcss()],
-  base: '/Nxus/', // <<< ADICIONE APENAS ESTA LINHA
+  plugins: [react(), tailwindcss()],
+  // A linha 'base' foi removida porque você está publicando na raiz do domínio 'app.nexus-manual.com.br'
   resolve: {
     alias: {
       "@": path.resolve(__dirname, "./src"),
